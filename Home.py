161481d01167554a6cad8cb95a561466799c9690
Davis Azangu <daviskasore04@gmail.com>
2024-06-03 15:31:21 +0300

    create all pages for the application

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('Churn Predictor App')
+        
+name= st.text_input('What is your name?')
+
+st.write(f'Hello {name}')
+
+
+
+
+
+
--- src/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.write("""# My Interface
-         This is my first interface using streamlit. """)
-
-
-
-
-
