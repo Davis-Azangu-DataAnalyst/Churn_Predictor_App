3b9c8f74df900627109a823e244c8f67a49e7dfb
Davis Azangu <daviskasore04@gmail.com>
2024-06-04 14:06:12 +0300

    update

---
+++ 01_Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.set_page_config(
+    page_title= 'Home Page',
+    page_icon=':repeat: :wave:',
+    layout= 'wide'
+)
+
+
+st.title('Churn Predictor App')
+
+
+        
+
+
+
+
+
+
